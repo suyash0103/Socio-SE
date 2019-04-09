96c8416fc4cbbe26c7b39a1915164e0f06a44458
Shreyas Shankar <shreyas.shankar920@gmail.com>
2019-04-09 13:31:34 +0530

    report ready

--- Socio/accounts/tests.py
+++ Socio/accounts/tests.py
@@ -17,20 +17,6 @@ class TestAdminViews(TestCase):
         self.user = User.objects.create_user(
             username='shreyas', email='shreyas@gmail.com', password='pass1234')
 
-    def test_login_page(self):
-        request = self.factory.get('/login/')
-
-        request.user = AnonymousUser()
-
-
-        response = LoginView.as_view()(request)
-        result = self.assertEqual(response.status_code, 200)
-        print(Fore.YELLOW + "Testing Login Page")
-        print(Style.RESET_ALL)
-        if result == None:
-            print(Fore.GREEN + "Login page verified")
-            print(Style.RESET_ALL)
-
     def test_signup_page(self):
         request = self.factory.get('/signup/')
 
@@ -45,14 +31,14 @@ class TestAdminViews(TestCase):
             print(Fore.GREEN + "SignUp page verified")
             print(Style.RESET_ALL)
 
-    def test_createFace_withLogin(self):
+    def test_signup_invalid(self):
         request = self.factory.get('/createFace/')
 
-        request.user = self.user
+        request.user = AnonymousUser()
 
 
         response = createFace(request)
-        print(Fore.YELLOW + "Testing createFace Page with login")
+        print(Fore.YELLOW + "Testing Signup Page with invalid credentials")
         print(Style.RESET_ALL)
         try:
             self.assertEqual(response.status_code, 200)
@@ -60,90 +46,42 @@ class TestAdminViews(TestCase):
             print(Style.RESET_ALL)
 
         except:
-            print(Fore.RED + "createFace page not working without login")
+            print(Fore.RED + "Signup Failed")
             print(Style.RESET_ALL)
 
-    def test_createFace_withoutLogin(self):
-        request = self.factory.get('/createFace/')
+    def test_login_page(self):
+        request = self.factory.get('/login/')
 
         request.user = AnonymousUser()
 
 
-        response = createFace(request)
-        print(Fore.YELLOW + "Testing createFace Page without login")
-        print(Style.RESET_ALL)
-        try:
-            self.assertEqual(response.status_code, 200)
-            print(Fore.GREEN + "createFace page verified")
-            print(Style.RESET_ALL)
-
-        except:
-            print(Fore.RED + "createFace page not working without login")
-            print(Style.RESET_ALL)
-
-    def test_createFaceEncoding_withLogin(self):
-        request = self.factory.get('/createFaceEncoding/')
-
-        request.user = self.user
-
-
-        response = createFaceEncoding(request)
-        print(Fore.YELLOW + "Testing createFaceEncoding Page with login")
-        print(Style.RESET_ALL)
-        try:
-            self.assertEqual(response.status_code, 200)
-            print(Fore.GREEN + "createFaceEncoding page verified")
-            print(Style.RESET_ALL)
-
-        except:
-            print(Fore.RED + "createFaceEncoding page not working without login")
-            print(Style.RESET_ALL)
-
-    def test_createFaceEncoding_withoutLogin(self):
-        request = self.factory.get('/createFaceEncoding/')
-
-        request.user = AnonymousUser()
-
-
-        response = createFaceEncoding(request)
-        print(Fore.YELLOW + "Testing createFaceEncoding Page without login")
-        print(Style.RESET_ALL)
-        try:
-            self.assertEqual(response.status_code, 200)
-            print(Fore.GREEN + "createFaceEncoding page verified")
-            print(Style.RESET_ALL)
-
-        except:
-            print(Fore.RED + "createFaceEncoding page not working without login")
-            print(Style.RESET_ALL)
-
-
-    def test_loginFace(self):
-        request = self.factory.get('/loginFace/')
-
-        request.user = self.user
-
-
-        response = loginFace(request)
-        print(Fore.YELLOW + "Testing loginFace Page")
-        print(Style.RESET_ALL)
-        self.assertEqual(response.status_code, 200)
-        print(Fore.GREEN + "loginFace page verified")
-        print(Style.RESET_ALL)
-
-    def test_face(self):
-        request = self.factory.get('/face/')
-
-        request.user = AnonymousUser()
-
-
-        response = loginFace(request)
+        response = LoginView.as_view()(request)
         result = self.assertEqual(response.status_code, 200)
-        print(Fore.YELLOW + "Testing Face Page")
+        print(Fore.YELLOW + "Testing Login Page")
         print(Style.RESET_ALL)
         if result == None:
-            print(Fore.GREEN + "Face page verified")
+            print(Fore.GREEN + "Login page verified")
             print(Style.RESET_ALL)
+
+    def test_login_invalid(self):
+        request = self.factory.get('/createFace/')
+
+        request.user = AnonymousUser()
+
+
+        response = createFace(request)
+        print(Fore.YELLOW + "Testing Login Page with invalid credentials")
+        print(Style.RESET_ALL)
+        try:
+            self.assertEqual(response.status_code, 200)
+            print(Fore.GREEN + "createFace page verified")
+            print(Style.RESET_ALL)
+
+        except:
+            print(Fore.RED + "Login failed")
+            print(Style.RESET_ALL)
+
+    
 
     def test_logout(self):
         # Create an instance of a GET request.
